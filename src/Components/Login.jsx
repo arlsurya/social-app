ed6b983ae41bda756064a275d3f0de71bca7fe0a
arlsurya <suryaaryal204@gmail.com>
2023-07-21 17:44:46 +0545

    login component comments added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,35 +3,40 @@ import '../Components/Login.css'
 
 function Login() {
 
+  // getting email from the user input
   const [email, setEmail] = useState('')
+  // getting password from the user input
   const [password, setPassword] = useState('')
 
-  const handleSubmit = ()=>{
-
-  const loginData = {
-    email:email,
-    password:password
-  }
-  console.log(loginData)
+  // when user hit login button
+  const handleSubmit = () => {
+    const loginData = {
+      email: email,
+      password: password
+    }
+    console.log(loginData)
   }
 
 
   return (
     <div className="main">
-    <div className='container'>
-      <div className="login-title">Login</div>
-      <div className="login-section">
-        <label className='label' htmlFor="email">Email:</label><br />
-        <input value={email} onChange={(e)=>setEmail(e.target.value)} className='form-control' type="email" /><br />
-        <label className='label' htmlFor="password">Password:</label><br />
-        <input value={password} onChange={(e)=>setPassword(e.target.value)} className='form-control' type="password" />
-        <div className="loginBtn">
-        <button onClick={handleSubmit} className='btn'>Login</button>
+      <div className='container'>
+        <div className="login-title">Login</div>
+        <div className="login-section">
+          <label className='label' htmlFor="email">Email:</label><br />
+          {/* onChange event to to get user email input value */}
+          <input value={email} onChange={(e) => setEmail(e.target.value)} className='form-control' type="email" /><br />
+          <label className='label' htmlFor="password">Password:</label><br />
+          {/* onChange event to to get user password value */}
+          <input value={password} onChange={(e) => setPassword(e.target.value)} className='form-control' type="password" />
+          <div className="loginBtn">
+            {/* handleSubmit function defined on top */}
+            <button onClick={handleSubmit} className='btn'>Login</button>
 
+          </div>
         </div>
-      </div>
 
-    </div>
+      </div>
     </div>
   )
 }
