7024a8f066bb29c518087d0bfb16e42030b71444
arlsurya <suryaaryal204@gmail.com>
2023-07-21 17:18:29 +0545

    login section created

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../Components/Login.css'
 
 function Login() {
+
   return (
-    <div>Login</div>
+    <div className='container'>
+      <div className="login-title">Login</div>
+      <div className="login-section">
+        <label className='label' htmlFor="email">Email:</label><br />
+        <input className='form-control' type="email" /><br />
+        <label className='label' htmlFor="password">Password:</label><br />
+        <input className='form-control' type="password" />
+      </div>
+
+    </div>
   )
 }
 
