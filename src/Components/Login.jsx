49b2999c0eeb3b9368fecc1704f31c15ddb149be
arlsurya <suryaaryal204@gmail.com>
2023-07-21 17:39:19 +0545

    login method added and user login information storing on state

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Components/Login.css'
 
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = ()=>{
+
+  const loginData = {
+    email:email,
+    password:password
+  }
+  console.log(loginData)
+  }
+
 
   return (
     <div className="main">
@@ -9,11 +22,11 @@ function Login() {
       <div className="login-title">Login</div>
       <div className="login-section">
         <label className='label' htmlFor="email">Email:</label><br />
-        <input className='form-control' type="email" /><br />
+        <input value={email} onChange={(e)=>setEmail(e.target.value)} className='form-control' type="email" /><br />
         <label className='label' htmlFor="password">Password:</label><br />
-        <input className='form-control' type="password" />
+        <input value={password} onChange={(e)=>setPassword(e.target.value)} className='form-control' type="password" />
         <div className="loginBtn">
-        <button className='btn'>Login</button>
+        <button onClick={handleSubmit} className='btn'>Login</button>
 
         </div>
       </div>
