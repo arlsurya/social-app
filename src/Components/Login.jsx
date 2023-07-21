9020e892dcca487a0d8649c373f7a13c5a74f575
arlsurya <suryaaryal204@gmail.com>
2023-07-21 17:23:42 +0545

    login component css updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,6 +4,7 @@ import '../Components/Login.css'
 function Login() {
 
   return (
+    <div className="main">
     <div className='container'>
       <div className="login-title">Login</div>
       <div className="login-section">
@@ -11,8 +12,13 @@ function Login() {
         <input className='form-control' type="email" /><br />
         <label className='label' htmlFor="password">Password:</label><br />
         <input className='form-control' type="password" />
+        <div className="loginBtn">
+        <button className='btn'>Login</button>
+
+        </div>
       </div>
 
+    </div>
     </div>
   )
 }
