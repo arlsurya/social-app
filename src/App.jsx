871f2dfd2dcac23a6ae2a5173da8c2a5ac294ab8
Navin Maharjan <maharjan.nabin85@gmail.com>
2023-07-23 10:16:24 +0545

    login page creadted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Footer from './Components/Footer'
+import Login from './Components/Login'
+
 
 function App() {
   return (
     <>
-    <Footer/>
+      <Login />
     </>
   )
 }
