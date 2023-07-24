c5290c25a5651b520c410038b7c7f4389cfb6551
arlsurya <suryaaryal204@gmail.com>
2023-07-24 18:24:38 +0545

    routes defined and home component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Login from './Components/Login'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Login />} />
+          <Route path="/forgotpassword" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+      
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
