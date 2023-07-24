dc4c82959b69ea52c12eaa0cace236b451d98487
arlsurya <suryaaryal204@gmail.com>
2023-07-24 19:13:21 +0545

    .env file setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
-
 const logger = require('morgan')
+const dotenv = require('./env')
+const app = express()
 
 app.use(cors())
 app.use(express.json())
