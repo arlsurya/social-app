b5f02a2c4c89bdbd57e641408d124aab4fe43f45
Navin Maharjan <129166934+navinmaharjan@users.noreply.github.com>
2023-07-25 06:49:14 +0545

    header footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from './Components/Header'
 import Login from './Components/Login'
+import Footer from './Components/Footer'
 
 function App() {
   return (
-    <div>
+    <div className=' bg-gradient-to-t from-cyan-500 to-blue-500 h-screen flex justify-center items-center flex-col gap-10'>
+      <Header />
       <Login />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-sm text-gray-900'>Copyright @socialapp 2023</div>
+  )
+}
+
+export default Footer
