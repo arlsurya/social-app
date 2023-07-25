2aaeb93279ad0be4cfe5b30a046e339246f5f764
Navin Maharjan <129166934+navinmaharjan@users.noreply.github.com>
2023-07-25 13:18:56 +0545

    basic homepage layout

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,14 +5,15 @@ function Home() {
     return (
         <>
             <div className='bg-gradient-to-t from-cyan-500 to-blue-500 w-full h-screen flex justify-center items-center px-8'>
-                <div className='w-[500px] h-[700px] bg-gray-200 rounded-lg'>
-                    <div>
+                <div className='w-[500px] h-[700px] bg-gray-200 rounded-lg flex flex-col justify-between overflow-hidden'>
+                    <div className=''>
                         <div className='flex flex-row items-center gap-10 p-8'>
                             <div className='flex flex-col items-center'>
                                 <div className='w-20 h-20 rounded-full overflow-hidden'>
                                     <img src={userphoto} alt="" className='w-full' />
                                 </div>
                                 <p className='text-sm text-gray-500'>Amanda</p>
+
                             </div>
                             <div className='w-3/5 bg-blue-400 px-5 rounded-lg'>
                                 <p>Hello, How Are You?</p>
@@ -31,11 +32,14 @@ function Home() {
                             </div>
                         </div>
 
-                        
+                      
                     </div>
 
-                    <div className='w-full h-14 bg-red-400 items-end mt-72'>
-
+                    <div className='w-full h-14 bg-gray-300 items-end mt-72 border-2'>
+                        <div className='w-full h-full flex flex-row items-center gap-2'>
+                            <input type="text" className='w-full border-0 h-10 ps-2' placeholder='Ping your message' />
+                           <button className='p-2 bg-blue-700 text-white rounded-xl hover:bg-blue-500'>Ping</button>
+                        </div>
                     </div>
                 </div>
 
