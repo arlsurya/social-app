5dacc963e3ebd1beb556fae50c314377540d9847
arlsurya <suryaaryal204@gmail.com>
2023-07-26 22:24:06 +0545

    prajwol header added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from './Components/Home';
 import LandingPage from './Components/LandingPage';
 import Register from './Components/Register';
 import ForgotPassword from './Components/ForgotPassword';
+import Header from './Components/Header';
 
 function App() {
   return (
     <Router>
       <div className='App'>
+        <Header/>
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
           <Route path="/login" element={<Login />} />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import "../Components/Header1.css"
 function Header() {
   return (
-    <div>
-      <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+    <div className='div'>
+      <ul className='ul'>
+            <li><a className='a' href="/">Home</a></li>
+            <li><a className='a' href="/about">About</a></li>
+            <li><a className='a' href="/contact">Contact</a></li>
             <li></li>
           </ul>
     </div>
